5d6a1ac44989e39ec29a5e4fbfcd1a74b714b3c3
Finrod Felagund <finrod.felagund.97@gmail.com>
2024-06-09 13:43:56 +0200

    dynamically group jobs in views based on dsl.yaml grouping

--- jobs/dsl.groovy
+++ jobs/dsl.groovy
@@ -12,7 +12,7 @@ def createJobs(yamlData) {
     yamlData.each { key, value ->
         if (value.getClass() == String) {
             //  this value of type String represents job names separated by whitespace
-            def jobnames = value.tokenize(' ')
+            def jobnames = value.tokenize(" ")
             for (jobname in jobnames) {
                 println("inside createJobs function: creating ${jobname} job")
                 job(jobname) {
@@ -28,8 +28,68 @@ def createJobs(yamlData) {
     }
 }
 
-String yamlFilePath = 'jobs/dsl.yaml'
-yamlData = readYamlFile(yamlFilePath)
+def addView(_context, _name, _content) {
+    println("***** Adding single view: ${_name} *****")
+    _context.listView(_name) {
+        description(_name) // TODO read description from YAML file
+        filterBuildQueue()
+        filterExecutors()
+        jobs {
+            // include all jobs who names end with "_name"
+            regex(".*${_name}")
+        }
+        columns {
+            status()
+            weather()
+            name()
+            lastSuccess()
+            lastFailure()
+            lastDuration()
+            buildButton()
+        }
+    }
+}
 
-def yamlType = yamlData.getClass()
-createJobs(yamlData)
+def addNestedView(_context, _name, _content) {
+    println("***** Adding nested view: ${_name} *****");
+    String __defaultView = "";
+    _context.nestedView(_name) {
+        views {
+            _content.each { key, value ->
+                // get name of default view
+                if (__defaultView == "")
+                {
+                    println("***** Default view ${key} for nested view: ${_name} *****");
+                    __defaultView = key;
+
+                }
+
+                // single view
+                if (value.getClass() == String) {
+                    addView(delegate, key, value);
+                }
+                // nested view
+                else {
+                    addNestedView(delegate, key, value);
+                }
+            }
+        }
+        configure { nview ->
+            nview / defaultView(__defaultView); //
+        }
+    }
+}
+
+String yamlFilePath = "jobs/dsl.yaml";
+yamlData = readYamlFile(yamlFilePath);
+
+def yamlType = yamlData.getClass();
+createJobs(yamlData);
+
+
+yamlData.each { key, value ->
+    if (value.getClass() == String)
+        addView(delegate, key, value);
+    else
+        addNestedView(delegate, key, value);
+}
