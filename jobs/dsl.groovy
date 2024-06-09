510d51ab0abe11f6eec40f66f1b2f8ac2a11ff74
Finrod Felagund <finrod.felagund.97@gmail.com>
2024-06-09 12:45:32 +0200

    dynamically create jobs from yaml file dsl.yaml

--- jobs/dsl.groovy
+++ jobs/dsl.groovy
@@ -8,15 +8,28 @@ def readYamlFile(String filePath) {
     return data
 }
 
+def createJobs(yamlData) {
+    yamlData.each { key, value ->
+        if (value.getClass() == String) {
+            //  this value of type String represents job names separated by whitespace
+            def jobnames = value.tokenize(' ')
+            for (jobname in jobnames) {
+                println("inside createJobs function: creating ${jobname} job")
+                job(jobname) {
+                    steps {
+                        shell("exec jobs/scripts/${jobname}/build.sh")
+                    }
+                }
+            }
+        }
+        else if (value.getClass() == yamlData.getClass()) {
+            createJobs(value)
+        }
+    }
+}
+
 String yamlFilePath = 'jobs/dsl.yaml'
 yamlData = readYamlFile(yamlFilePath)
 
-yamlData.each { key, value ->
-    println("${key}: ${value}")
-}
-
-job('example') {
-  steps {
-    shell('echo Hello World!')
-  }
-}
+def yamlType = yamlData.getClass()
+createJobs(yamlData)
