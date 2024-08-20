9355caf5763acd15a40b81031b59e71179019bb1
Finrod Felagund <finrod.felagund.97@gmail.com>
2024-08-20 15:38:07 +0200

    fix syntax in docker build command for all ubuntu images

--- jobs/scripts/ubuntu-focal-docker/build.sh
+++ jobs/scripts/ubuntu-focal-docker/build.sh
@@ -2,6 +2,6 @@
 
 export SCRIPTPATH=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
-docker build -t gitcitadel-ubuntu-focal -f $SCRIPTPATH/Dockerfile
+docker build -t gitcitadel-ubuntu-focal -f $SCRIPTPATH/Dockerfile .
 docker tag gitcitadel-ubuntu-focal docker.gitcitadel.eu/gitcitadel-ubuntu-focal:latest
 docker push docker.gitcitadel.eu/gitcitadel-ubuntu-focal:latest
--- jobs/scripts/ubuntu-jammy-docker/build.sh
+++ jobs/scripts/ubuntu-jammy-docker/build.sh
@@ -2,6 +2,6 @@
 export SCRIPTPATH=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
 
-docker build -t gitcitadel-ubuntu-jammy -f $SCRIPTPATH/Dockerfile
+docker build -t gitcitadel-ubuntu-jammy -f $SCRIPTPATH/Dockerfile .
 docker tag gitcitadel-ubuntu-jammy docker.gitcitadel.eu/gitcitadel-ubuntu-jammy:latest
 docker push docker.gitcitadel.eu/gitcitadel-jammy-noble:latest
