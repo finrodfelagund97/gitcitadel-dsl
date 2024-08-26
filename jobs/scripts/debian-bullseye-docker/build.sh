ca6b3606662ffc7255ae613d8aa4ecdac7fe94f0
Finrod Felagund <finrod.felagund.97@gmail.com>
2024-08-26 16:44:41 +0200

    write script to build docker image based on debian bullseye

--- jobs/scripts/debian-bullseye-docker/build.sh
+++ jobs/scripts/debian-bullseye-docker/build.sh
@@ -2,6 +2,6 @@
 
 export SCRIPTPATH=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
-docker build -t gitcitadel-debian-buster -f $SCRIPTPATH/Dockerfile .
-docker tag gitcitadel-debian-buster docker.gitcitadel.eu/gitcitadel-debian-buster:latest
-docker push docker.gitcitadel.eu/gitcitadel-debian-buster:latest
+docker build -t gitcitadel-debian-bullseye -f $SCRIPTPATH/Dockerfile .
+docker tag gitcitadel-debian-bullseye docker.gitcitadel.eu/gitcitadel-debian-bullseye:latest
+docker push docker.gitcitadel.eu/gitcitadel-debian-bullseye:latest
